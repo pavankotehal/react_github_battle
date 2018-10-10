f939e9f5e45d41ee70a3d59f7a4bb3447e6ffecb
Pavan Kotehal <pavan.kotehal@gmail.com>
2018-10-10 19:04:24 +1100

    Dev branch

--- app/index.js
+++ app/index.js
@@ -6,13 +6,31 @@ class App extends React.Component{
     render(){
         return (
             <div>
-                Hello World
+                <h2>Hello, {this.props.name}</h2>
+            </div>
+        )
+    }
+}
+
+class App2 extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>
+                    {this.props.text}
+                </p>
+                <button>Submit</button>
             </div>
         )
     }
 }
 
 ReactDom.render(
-    <App/>,
+    <App name="Pavan Kotehal Parameshwarappa"/>,
     document.getElementById('app')
+)
+
+ReactDom.render(
+    <App2 text="test test test test test test test test test"/>,
+    document.getElementById('app2')
 )
